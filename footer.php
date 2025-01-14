74e77f4ae5b5117f5da7e1ecd9eb00ab48caef11
Avinashkdm10 <avinash.prixim@gmail.com>
2025-01-14 15:16:02 +0530

    content

--- footer.php
+++ footer.php
@@ -84,7 +84,7 @@
                                     <a href="contact-us.php">Contact Us</a>
                                 </li>
                                 <li>
-                                    <a href="https://forexoutsource.wordpress.com/">Blogs</a>
+                                    <a href="https://forexoutsource.com/blogs/">Blogs</a>
                                 </li>
                             </ul>
                             <h3>Explore Us</h3>
